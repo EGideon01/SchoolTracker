086e65d1223496c1c61fd3ac1eedcc4dc16975af
Nutulous <127353946+Nutulous@users.noreply.github.com>
2023-06-02 11:01:35 -0500

    Update Assignment.js

--- Assignment.js
+++ Assignment.js
@@ -1,28 +1,28 @@
 import React, { useState } from 'react';
 import './App.css';
 import { v4 as uuidv4 } from 'uuid';
-uuidv4();
+
 
 const Assignment = () => {
-  const [input, setInput] = useState("")
+  const [input, setInput] = useState('')
   const [submittedInput, setSubmittedInput] = useState([])
-
+    
   const handleChange = (e) => {
     setInput(e.target.value) // Updates state every key stroke
   }
   
   const handleClick = () => {
-    setSubmittedInput(input) // updates our list of submitted input with the typed word from the state "input"
+    setSubmittedInput(submittedInput.concat([input])) // updates our list of submitted input with the typed word from the state "input"
   }
 
   const inputList = submittedInput.map((input) => {
-    <p key={uuidv4}>{input}</p>
-  }) // We have to insert a "key" for each list item, otherwise react spits out errors
-
+    <p key={uuidv4()}>{input}</p>
+    }) // We have to insert a "key" for each list item, otherwise react spits out errors
+  
   return(
     <div id="Assignment">
-      <input type="text" onchange={handleChange}/><input/>
-      <button type="submit" onclick={handleClick}>Submit</button>
+      <input type="text" onChange={handleChange}/><input/>
+      <button type="submit" onClick={handleClick}>Submit</button>
       <div id="InputArea">
         {inputList}
       </div>
