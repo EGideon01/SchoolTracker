4e95f8a8fb32a7652314073d47273fccc952242b
EspenGideon <127353946+EGideon01@users.noreply.github.com>
2023-07-14 09:20:15 -0500

    Adds the Delete All Values Button (Assignment.js)
    Adds a seperate button to delete the current array values chosen by the user. Not completely necessary but will yield useful once these values are stored within a server.

--- src/Assignment.js
+++ src/Assignment.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import './Assignment.css';
 import { v4 as uuidv4 } from 'uuid';
+import { render } from '@testing-library/react';
 
 
 const Assignment = () => {
@@ -15,17 +16,22 @@ const Assignment = () => {
     setSubmittedInput(submittedInput.concat([input])) // updates our list of submitted input with the typed word from the state "input"
   }
 
-  const inputList = submittedInput.map((input) => {
+  const inputList = submittedInput.map((input) => 
     <p key={uuidv4()}>{input}</p>
-    }) // We have to insert a "key" for each list item, otherwise react spits out errors
+    ) // We have to insert a "key" for each list item, otherwise react spits out errors
   
+    const clearedArray = () => { 
+      setSubmittedInput(submittedInput.filter(input => input.id != null))
+    }
+    
   return(
     <div id="Assignment">
-      <input type="text" onChange={handleChange}/><input/>
+      <input type="text" onChange={handleChange}/>
       <button type="submit" onClick={handleClick}>Submit</button>
       <div id="InputArea">
         {inputList}
       </div>
+      <button type="submit" onClick={clearedArray}>Delete All Values</button>
     </div>
   )
 }
